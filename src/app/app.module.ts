800ddc751979942ea269567695877b993597c97d
Me <fox.derrick@gmail.com>
2020-02-13 12:15:46 -0500

    finished basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { MainLayerComponent } from './main-layer/main-layer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ReviewsSectionComponent } from './reviews-section/reviews-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerComponent,
-    MainLayerComponent
+    MainLayerComponent,
+    NavBarComponent,
+    FooterComponent,
+    ReviewsSectionComponent
   ],
   imports: [
     BrowserModule
