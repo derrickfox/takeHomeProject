e739a309b8e7d2d8be1ea3d77d76f414d1cfe3cb
Me <fox.derrick@gmail.com>
2020-02-13 12:08:41 -0500

    basic structure save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { MainLayerComponent } from './main-layer/main-layer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    MainLayerComponent
   ],
   imports: [
     BrowserModule
