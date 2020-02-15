d526ef5ee0aade2c15ed3a32dcf551b702a0772d
Me <fox.derrick@gmail.com>
2020-02-14 19:47:15 -0500

    reviews list

--- src/app/reviews-section/reviews-section.component.ts
+++ src/app/reviews-section/reviews-section.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { Review } from './reviews/review.model';
 
 @Component({
-  selector: 'app-reviews-section',
-  templateUrl: './reviews-section.component.html',
-  styleUrls: ['./reviews-section.component.scss']
+	selector: 'app-reviews-section',
+	templateUrl: './reviews-section.component.html',
+	styleUrls: ['./reviews-section.component.scss']
 })
 export class ReviewsSectionComponent implements OnInit {
 
-  constructor() { }
+	reviews: Review[] = [
+		new Review(
+			'../../../assets/image-assets/reviewer1.png',
+			'Shaun White',
+			'This Thing is a monster!',
+			'Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test',
+			5
+		),
+		new Review(
+			'../../../assets/image-assets/reviewer2.png',
+			'Jennie Loren',
+			'Luv it so much I bought 2!',
+			'Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test',
+			3
+		)
+	]
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
--- src/app/reviews-section/reviews/reviews.component.ts
+++ src/app/reviews-section/reviews/reviews.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Review } from './review.model';
 
 @Component({
-  selector: 'app-reviews',
-  templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.scss']
+	selector: 'app-reviews',
+	templateUrl: './reviews.component.html',
+	styleUrls: ['./reviews.component.scss']
 })
 export class ReviewsComponent implements OnInit {
+	@Input() review: Review;
 
-  constructor() { }
+	constructor() {
 
-  ngOnInit() {
-  }
+	}
+
+	ngOnInit() {
+
+	}
 
 }
