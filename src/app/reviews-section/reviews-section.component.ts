cf181f8fe6123822b7ffe09f586c41e7bfc4409d
Me <fox.derrick@gmail.com>
2020-02-17 15:43:32 -0500

    added content for review 2

--- src/app/reviews-section/reviews-section.component.ts
+++ src/app/reviews-section/reviews-section.component.ts
@@ -24,16 +24,19 @@ export class ReviewsSectionComponent implements OnInit {
 			want a jacket that dresses up nice. With slacks, nice shoes, and a button up it looks great. I am 6' and about 190 lbs (34 in
 			arms) and the large fits perfect. The pockets are well designed as well. The soft shell is the new 'do-everything' jacket:
 			wind/water resistent but also maleable and comfy for wearing anywhere. I highly recommend this jacket.`,
-			[1, 2, 3, 4, 5],
-			'08/23/19'
+			[1, 2, 3, 4],
+			'08/20/2013'
 		),
 		new Review(
 			'../assets/image-assets/reviewer2.png',
 			'Jennie Loren',
 			'Luv it so much I bought 2!',
-			'Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test',
+			`Love this jacket so much from previous years I decided to buy a second in a different color. I'm tall and fairly slender and
+			its tailored silhouette fits me nicely. For perspective I'm around 6'3" ~150 lbs, and a large does the trick. I'll wear this for all of
+			fall and into the winter. Despite not being bulky or baggy I find it good and warm for anything down to 40 degrees and
+			perhaps lower if you're keeping up on your layering. For weather at freezing or below you should consider something heavier.`,
 			[1, 2, 3],
-			'11/21/19'
+			'01/12/2013'
 		)
 	]
 
